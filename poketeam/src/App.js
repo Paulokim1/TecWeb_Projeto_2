69e937f8d858ba6defda1c06f1370615e1d55fdd
paulokim1 <paulo.wook21@gmail.com>
2021-11-07 18:08:54 -0300

    subindo projeto

---
+++ poketeam/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src='poketeam.png' />
+      </div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ poketeam/src/components/Home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+export default function Home(props) {
+  return (
+    <div className = 'homeContainer'>
+        <button className = 'btn'>
+          Criar
+        </button>
+
+        <button className = 'btn'> 
+          Times
+        </button>
+      </div>
+  );
+}
