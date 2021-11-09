c26e4d8d2b1743ea81a8d6720e6ef60c44f60f6a
Rodrigo Nigri Griner <rodrigong1@al.insper.edu.br>
2021-11-08 21:16:57 -0300

    componente routes testada e funcionando

--- poketeam/src/App.js
+++ poketeam/src/App.js
@@ -1,16 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavLink } from "react-router-dom";
 import Home from "./components/Home";
 import Team from "./components/Team";
 import Teams from "./components/Teams";
 import Header from "./components/Header";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <Header className="App-header"/>
-      <Home />
-    </div>
+
+      <div className="App">
+        <Header className="App-header"/>
+        <Routes>
+          <Route path = "/" element = {<Home />} />
+          <Route path = "/teams" element = {<Teams />} />
+          <Route path = "/team" element = {<Team />} />
+            
+        </Routes>
+      </div>
+
   );
 }
 
--- poketeam/src/components/Home/index.js
+++ poketeam/src/components/Home/index.js
@@ -1,16 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./index.css";
 
 export default function Home(props) {
   return (
     <div className = 'homeContainer'>
+      <Link to = "/team">
         <button className = 'btn'>
           Criar
         </button>
+      </Link>
 
+      <Link to = "/teams">
         <button className = 'btn'> 
           Times
         </button>
+      </Link>
+      
       </div>
   );
 }
